f3feb3a888c73769c02539e693eae308d41dec58
Belal Elsabbagh <belsabbagh@gmail.com>
2024-04-13 18:51:13 +0200

    Refactor assignment-problem.py to remove hierarchical_clustering import

--- assignment-problem.py
+++ assignment-problem.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 import pandas as pd
-from src.cluster import KMeans, hierarchical_clustering
+from src.cluster import KMeans
 
 
 df = pd.read_csv("data/assignment-example.csv", index_col="id")
@@ -11,5 +11,5 @@ res = [model.predict(i) for i in df.values]
 plt.scatter(df["x"], df["y"], c=res, cmap="viridis")
 plt.scatter(model.center_dim(0), model.center_dim(1), c="red", marker="x")
 for i in df.index:
-    plt.annotate(i, (df["x"][i] + 0.02, df["y"][i] + 0.02))
+    plt.annotate(i, (df["x"][i].item() + 0.02, df["y"][i].item() + 0.02))
 plt.show()
